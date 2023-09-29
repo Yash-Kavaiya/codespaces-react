7282e8b9a766ded3034b13bd9818013e5f93f2c8
Yash Kavaiya <103239923+Yash-Kavaiya@users.noreply.github.com>
2023-09-29 17:29:26 +0000

    Demo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './resources/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+import Header from './components/header_footer/Header.jsx';
+import Main from './components/core/Main.jsx';
+import Footer from './components/header_footer/Footer.jsx';
+
+class App extends React.Component {
+    render() {
+      return (
+        <div className="App">
+            <Header/>
+            <Main />
+            <Footer/>
+        </div>
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(<App/>, document.getElementById('root'));
